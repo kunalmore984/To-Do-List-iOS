5726258fef892ad138c031854d4ec3d429b1e308
Kunal More <kunalmore2288@gmail.com>
2024-10-03 20:11:22 -0400

    View for each list items

--- To-Do-List(iOS)/ListView.swift
+++ To-Do-List(iOS)/ListView.swift
@@ -1,101 +1,103 @@
-//
-//  ListView.swift
-//  To-Do-List(iOS)
-//
-//  Created by kunal more on 24/09/24.
-//
-
 import SwiftUI
+import SwiftData
 
 struct ListView: View {
-    
-    struct ItemName: Identifiable {
-        let id = UUID()
-        var item_name: String
-        var item_count: Int
-    }
-    struct Items {
-        var item_names: [ItemName] = []
-        
-    }
-    @State private var items = Items()
+    // Filter by list
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) var dismiss
     @State private var itemname: String = ""
     @State private var isAddingItem: Bool = false
-    @State var list_name: String
+    @State var list: ListViewModel
+    
     var body: some View {
-        VStack{
-            HStack{
-                Image(systemName: "pen")
-                    .imageScale(.large)
-                    .foregroundStyle(.purple)
-                Text(list_name)
-                    .font(.title)
-                    .bold()
-            }.padding()
-            
-            Button{
-                self.isAddingItem = true
-            } label: {
-                Text("➕ Add Item")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.purple)
-                    .cornerRadius(20)
-            }
-            List{
-                ForEach(items.item_names){ itemn in
-                    Itemname(Item_Name: itemn)
-                }
-            }
-            if isAddingItem{
-                HStack{
-                    TextField("Enter Item Name", text: $itemname)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding()
-                    
-                    Button("Save"){
-                        if !self.itemname.isEmpty {
-                            self.items.item_names.append(ItemName(item_name: itemname, item_count: 0))
-                            self.itemname = ""
-                            self.isAddingItem = false
-                        }
-                    }
-                    .padding()
-                    .foregroundStyle(.purple)
-                    .cornerRadius(20)
+        NavigationStack {
+            VStack {
+                HStack {
+                    Image(systemName: "note")
+                        .imageScale(.large)
+                        .foregroundColor(.green)
+                    Text(list.list_name)
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.green)
                 }
                 .padding()
+                
+                if list.items.isEmpty {
+                    ContentUnavailableView("Add Task in this \(list.list_name)", systemImage: "list.bullet.clipboard.fill")
+                        .foregroundColor(.green)
+                    
+                    if isAddingItem {
+                        taskInputField
+                    }
+                } else {
+                    List {
+                        ForEach(list.items.indices, id: \.self) { item in
+                            HStack {
+                                Text(list.items[item])
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                                Spacer()
+                            }
+                        }.onDelete{indexSet in
+                            indexSet.forEach{ index in
+                                list.items.remove(at: index)
+                                self.list.item_count -= 1
+                            }
+                        }
+                        if isAddingItem {
+                            taskInputField
+                        }
+                    }.listStyle(.plain)
+                }
             }
+            .toolbar {
+                toolbarContent
+            }
+            .padding()
         }
-        .padding()
     }
     
-    struct Itemname: View {
-        var Item_Name: ItemName
-        
-        var body: some View {
-            VStack(alignment: .leading, spacing: 3){
-                HStack(spacing: 3){
-                    Text(Item_Name.item_name)
-                        .foregroundColor(.primary)
-                        .bold()
-                    Spacer()
-                    Button(action: {
-                        
-                    }){
-                        Image(systemName: "circle")
-                            .foregroundColor(.purple)
-                            .padding()
-                    }
+    // Input field for adding new tasks
+    private var taskInputField: some View {
+        HStack {
+            TextField("Enter Item Name", text: $itemname)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button("Save") {
+                if !self.itemname.isEmpty {
+                    self.list.items.append(itemname)
+                    self.list.item_count += 1
+                    self.itemname = "" // Clear input
+                    self.isAddingItem = false// Hide input field
                 }
-                .foregroundColor(.secondary)
-                .font(.subheadline)
-                Label(String(Item_Name.item_count),systemImage: "globe")
+            }
+            .padding(.leading)
+            .foregroundColor(.green)
+        }
+    }
+    
+    // Toolbar content for adding or canceling task addition
+    private var toolbarContent: some ToolbarContent {
+        ToolbarItemGroup(placement: .navigationBarTrailing) {
+            if isAddingItem {
+                Button {
+                    self.isAddingItem = false // Cancel adding task
+                } label: {
+                    Image(systemName: "multiply.circle.fill")
+                        .imageScale(.large)
+                        .foregroundColor(.green)
+                }
+            } else {
+                Button {
+                    self.isAddingItem = true // Show input field for adding task
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .imageScale(.large)
+                        .foregroundColor(.green)
+                }
             }
         }
     }
-}
-
-#Preview {
-    ListView(list_name: "List 1")
 }
