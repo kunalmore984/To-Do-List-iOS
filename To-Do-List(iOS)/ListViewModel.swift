fe65b5c04ef506d9f26d4d8bc16ab9570571c537
Kunal More <kunalmore2288@gmail.com>
2024-10-03 20:14:46 -0400

    Model for database to store a list and it's items

--- To-Do-List(iOS)/ListViewModel.swift
+++ To-Do-List(iOS)/ListViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class ListViewModel{
+    
+    var list_name: String
+    var items: [String] = []
+    var item_count: Int
+    
+    init(list_name: String,item_count: Int = 0) {
+        self.list_name = list_name
+        self.item_count = item_count
+    }
+}
